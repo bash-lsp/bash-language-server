15fe63438a08ca6e412e13c50e111ed8d8c1b296
Mads Hartmann <mads379@gmail.com>
2018-02-25 08:49:08 +0100

    Improve package.json for marketplace

---
+++ vscode-client/README.md
@@ -0,0 +1,21 @@
+# Bash IDE
+
+**WIP: This is still very much under development. I don't expect it to work on
+other computers than mine**
+
+Bash language server implementation based.
+
+Uses [Tree Sitter][tree-sitter] and it's [grammar for Bash][tree-sitter-bash].
+
+## Features
+
+- [x] Jump to declaration
+- [x] Find references
+- [x] Code Outline & Show Symbols
+- [x] Highlight occurrences
+- [x] Code completion
+- [x] Simple diagnostics reporting
+- [ ] Rename symbol
+
+[tree-sitter]: https://github.com/tree-sitter/tree-sitter
+[tree-sitter-bash]: https://github.com/tree-sitter/tree-sitter-bash
--- vscode-client/src/extension.ts
+++ vscode-client/src/extension.ts
@@ -9,8 +9,6 @@ export function activate(context: ExtensionContext) {
 
 	const serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
 		run : {
       command: "node",
@@ -22,22 +20,18 @@ export function activate(context: ExtensionContext) {
     }
 	}
 
-	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
 		documentSelector: [{
       scheme: 'file',
       language: 'shellscript'
     }],
 		synchronize: {
-			// Synchronize the setting section 'languageServerExample' to the server
 			configurationSection: 'Bash IDE',
 			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
 
-	// Create the language client and start the client.
 	let disposable = new LanguageClient('Bash IDE', 'Bash IDE', serverOptions, clientOptions).start();
 
 	// Push the disposable to the context's subscriptions so that the
