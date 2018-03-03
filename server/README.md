a807168ad783dfc3a10c278798c76f0e7bc193b0
Mads Hartmann <mads379@gmail.com>
2018-03-03 09:37:11 +0100

    Add executable to bash-language-server package
    Seeing as I can't figure out how to ship an extension that has native
    dependencies I'll instead try to have users npm i -g this package and
    then run the server completely outside the context of vscode.

--- server/README.md
+++ server/README.md
@@ -3,5 +3,8 @@
 Bash language server implementation based on [Tree Sitter][tree-sitter] and its
 [grammar for Bash][tree-sitter-bash].
 
+For more information see the main Github repository: [mads-hartmann/bash-language-server][repo]
+
 [tree-sitter]: https://github.com/tree-sitter/tree-sitter
 [tree-sitter-bash]: https://github.com/tree-sitter/tree-sitter-bash
+[repo]: https://github.com/mads-hartmann/bash-language-server
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import {
   createConnection,
@@ -7,149 +7,170 @@ import {
   InitializeResult,
   TextDocumentPositionParams,
   CompletionItem,
-	Definition,
+  Definition,
   StreamMessageReader,
   StreamMessageWriter,
   SymbolInformation,
   DocumentSymbolParams,
   Location,
   DocumentHighlight,
-  ReferenceParams} from 'vscode-languageserver';
+  ReferenceParams
+} from "vscode-languageserver";
 
-const glob = require('glob')
-const fs = require('fs')
-import * as Path from 'path'
-import * as Analyser from './analyser';
+const glob = require("glob");
+const fs = require("fs");
+import * as Path from "path";
 
-// Create a connection for the server.
-// The connection uses stdin/stdout for communication.
-const connection: IConnection = createConnection(
-  new StreamMessageReader(process.stdin),
-  new StreamMessageWriter(process.stdout)
-);
+import * as Analyser from "./analyser";
 
-// Create a simple text document manager. The text document manager
-// supports full document sync only
-const documents: TextDocuments = new TextDocuments();
+export function listen() {
+  // Create a connection for the server.
+  // The connection uses stdin/stdout for communication.
+  const connection: IConnection = createConnection(
+    new StreamMessageReader(process.stdin),
+    new StreamMessageWriter(process.stdout)
+  );
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-documents.listen(connection);
+  // Create a simple text document manager. The text document manager
+  // supports full document sync only
+  const documents: TextDocuments = new TextDocuments();
 
-connection.onInitialize((params): InitializeResult => {
-  connection.console.log(`Initialized for ${params.rootUri}, ${params.rootPath}`)
+  // Make the text document manager listen on the connection
+  // for open, change and close text document events
+  documents.listen(connection);
 
-  glob(
-    "**/*.sh",
-    {cwd: params.rootPath, },
-    (err, paths) => {
+  connection.onInitialize((params): InitializeResult => {
+    connection.console.log(
+      `Initialized for ${params.rootUri}, ${params.rootPath}`
+    );
+
+    glob("**/*.sh", { cwd: params.rootPath }, (err, paths) => {
       if (err != null) {
-        connection.console.error(err)
+        connection.console.error(err);
       } else {
         paths.forEach(p => {
-          const absolute = Path.join(params.rootPath, p)
-          const uri = 'file://' + absolute
-          connection.console.log('Analyzing ' + uri)
-          Analyser.analyze(uri, fs.readFileSync(absolute, 'utf8'))
-        })
+          const absolute = Path.join(params.rootPath, p);
+          const uri = "file://" + absolute;
+          connection.console.log("Analyzing " + uri);
+          Analyser.analyze(uri, fs.readFileSync(absolute, "utf8"));
+        });
       }
-    }
-  )
+    });
 
-	return {
-		capabilities: {
-			// For now we're using full-sync even though tree-sitter has great support
-			// for partial updates.
-			textDocumentSync: documents.syncKind,
-			completionProvider: {
-				resolveProvider: true
-      },
-      documentHighlightProvider: true,
-      definitionProvider: true,
-      documentSymbolProvider: true,
-      referencesProvider: true
-		}
-	}
-});
-
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
-documents.onDidChangeContent((change) => {
-  connection.console.log('Invoked onDidChangeContent');
-  const uri = change.document.uri
-  const contents = change.document.getText();
-  const diagnostics = Analyser.analyze(uri, contents)
-  connection.sendDiagnostics({
-    uri: change.document.uri,
-    diagnostics
-  })
-});
-
-connection.onDidChangeWatchedFiles((_change) => {
-	// Monitored files have change in VSCode
-	connection.console.log('We received an file change event');
-});
-
-connection.onDefinition((textDocumentPosition: TextDocumentPositionParams): Definition => {
-  connection.console.log(`Asked for definition at ${textDocumentPosition.position.line}:${textDocumentPosition.position.character}`);
-  const word = Analyser.wordAtPoint(
-    textDocumentPosition.textDocument.uri,
-    textDocumentPosition.position.line,
-    textDocumentPosition.position.character
-  )
-  return Analyser.findDefinition(word);
-});
-
-connection.onDocumentSymbol((params: DocumentSymbolParams): SymbolInformation[] => {
-  return Analyser.findSymbols(params.textDocument.uri)
-})
-
-connection.onDocumentHighlight((textDocumentPosition: TextDocumentPositionParams): DocumentHighlight[] => {
-  const word = Analyser.wordAtPoint(
-    textDocumentPosition.textDocument.uri,
-    textDocumentPosition.position.line,
-    textDocumentPosition.position.character
-  )
-  return Analyser
-    .findOccurrences(textDocumentPosition.textDocument.uri, word)
-    .map(n => ({range: n.range}))
-})
-
-connection.onReferences((params: ReferenceParams): Location[] => {
-  const word = Analyser.wordAtPoint(
-    params.textDocument.uri,
-    params.position.line,
-    params.position.character
-  )
-  return Analyser.findReferences(word)
-})
-
-connection.onCompletion((textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-  connection.console.log(`Asked for completions at ${textDocumentPosition.position.line}:${textDocumentPosition.position.character}`);
-  const symbols = Analyser.findSymbols(textDocumentPosition.textDocument.uri)
-  return symbols.map((s: SymbolInformation) => {
     return {
-			label: s.name,
-			kind: s.kind,
-      data: s.name // Used for later resolving more info.
+      capabilities: {
+        // For now we're using full-sync even though tree-sitter has great support
+        // for partial updates.
+        textDocumentSync: documents.syncKind,
+        completionProvider: {
+          resolveProvider: true
+        },
+        documentHighlightProvider: true,
+        definitionProvider: true,
+        documentSymbolProvider: true,
+        referencesProvider: true
+      }
+    };
+  });
+
+  // The content of a text document has changed. This event is emitted
+  // when the text document first opened or when its content has changed.
+  documents.onDidChangeContent(change => {
+    connection.console.log("Invoked onDidChangeContent");
+    const uri = change.document.uri;
+    const contents = change.document.getText();
+    const diagnostics = Analyser.analyze(uri, contents);
+    connection.sendDiagnostics({
+      uri: change.document.uri,
+      diagnostics
+    });
+  });
+
+  connection.onDidChangeWatchedFiles(_change => {
+    // Monitored files have change in VSCode
+    connection.console.log("We received an file change event");
+  });
+
+  connection.onDefinition(
+    (textDocumentPosition: TextDocumentPositionParams): Definition => {
+      connection.console.log(
+        `Asked for definition at ${textDocumentPosition.position.line}:${
+          textDocumentPosition.position.character
+        }`
+      );
+      const word = Analyser.wordAtPoint(
+        textDocumentPosition.textDocument.uri,
+        textDocumentPosition.position.line,
+        textDocumentPosition.position.character
+      );
+      return Analyser.findDefinition(word);
     }
-  })
-});
+  );
 
-// This handler resolve additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-  // TODO: Look up man pages for commands
-  // TODO: For builtins look up the docs.
-  // TODO: For functions, parse their comments?
+  connection.onDocumentSymbol(
+    (params: DocumentSymbolParams): SymbolInformation[] => {
+      return Analyser.findSymbols(params.textDocument.uri);
+    }
+  );
 
-	// if (item.data === 1) {
-	// 	item.detail = 'TypeScript details',
-	// 	item.documentation = 'TypeScript documentation'
-  // }
+  connection.onDocumentHighlight(
+    (textDocumentPosition: TextDocumentPositionParams): DocumentHighlight[] => {
+      const word = Analyser.wordAtPoint(
+        textDocumentPosition.textDocument.uri,
+        textDocumentPosition.position.line,
+        textDocumentPosition.position.character
+      );
+      return Analyser.findOccurrences(
+        textDocumentPosition.textDocument.uri,
+        word
+      ).map(n => ({ range: n.range }));
+    }
+  );
 
-	return item;
-});
+  connection.onReferences((params: ReferenceParams): Location[] => {
+    const word = Analyser.wordAtPoint(
+      params.textDocument.uri,
+      params.position.line,
+      params.position.character
+    );
+    return Analyser.findReferences(word);
+  });
 
-// Listen on the connection
-connection.listen();
+  connection.onCompletion(
+    (textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+      connection.console.log(
+        `Asked for completions at ${textDocumentPosition.position.line}:${
+          textDocumentPosition.position.character
+        }`
+      );
+      const symbols = Analyser.findSymbols(
+        textDocumentPosition.textDocument.uri
+      );
+      return symbols.map((s: SymbolInformation) => {
+        return {
+          label: s.name,
+          kind: s.kind,
+          data: s.name // Used for later resolving more info.
+        };
+      });
+    }
+  );
+
+  // This handler resolve additional information for the item selected in
+  // the completion list.
+  connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+    // TODO: Look up man pages for commands
+    // TODO: For builtins look up the docs.
+    // TODO: For functions, parse their comments?
+
+    // if (item.data === 1) {
+    // 	item.detail = 'TypeScript details',
+    // 	item.documentation = 'TypeScript documentation'
+    // }
+
+    return item;
+  });
+
+  // Listen on the connection
+  connection.listen();
+}
