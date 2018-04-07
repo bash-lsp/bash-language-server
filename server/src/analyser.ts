4d98f3763fe492e82d5960af69092c61df054b48
Mads Hartmann <mads379@gmail.com>
2018-04-07 15:05:03 +0200

    Add mapping from symbol kind to completion kind
    This was a change in the LSP interface.

--- server/src/analyser.ts
+++ server/src/analyser.ts
@@ -147,7 +147,7 @@ export default class Analyzer {
     return uniqueBasedOnHash(this.findSymbols(uri), hashFunction).map(
       (symbol: LSP.SymbolInformation) => ({
         label: symbol.name,
-        kind: symbol.kind,
+        kind: this.symbolKindToCompletionKind(symbol.kind),
         data: {
           name: symbol.name,
           type: 'function',
@@ -224,5 +224,57 @@ export default class Analyzer {
     }
 
     return name
+  }
+
+  private symbolKindToCompletionKind(s: LSP.SymbolKind): LSP.CompletionItemKind {
+    switch (s) {
+      case LSP.SymbolKind.File:
+        return LSP.CompletionItemKind.File
+      case LSP.SymbolKind.Module:
+      case LSP.SymbolKind.Namespace:
+      case LSP.SymbolKind.Package:
+        return LSP.CompletionItemKind.Module
+      case LSP.SymbolKind.Class:
+        return LSP.CompletionItemKind.Class
+      case LSP.SymbolKind.Method:
+        return LSP.CompletionItemKind.Method
+      case LSP.SymbolKind.Property:
+        return LSP.CompletionItemKind.Property
+      case LSP.SymbolKind.Field:
+        return LSP.CompletionItemKind.Field
+      case LSP.SymbolKind.Constructor:
+        return LSP.CompletionItemKind.Constructor
+      case LSP.SymbolKind.Enum:
+        return LSP.CompletionItemKind.Enum
+      case LSP.SymbolKind.Interface:
+        return LSP.CompletionItemKind.Interface
+      case LSP.SymbolKind.Function:
+        return LSP.CompletionItemKind.Function
+      case LSP.SymbolKind.Variable:
+        return LSP.CompletionItemKind.Variable
+      case LSP.SymbolKind.Constant:
+        return LSP.CompletionItemKind.Constant
+      case LSP.SymbolKind.String:
+      case LSP.SymbolKind.Number:
+      case LSP.SymbolKind.Boolean:
+      case LSP.SymbolKind.Array:
+      case LSP.SymbolKind.Key:
+      case LSP.SymbolKind.Null:
+        return LSP.CompletionItemKind.Text
+      case LSP.SymbolKind.Object:
+        return LSP.CompletionItemKind.Module
+      case LSP.SymbolKind.EnumMember:
+        return LSP.CompletionItemKind.EnumMember
+      case LSP.SymbolKind.Struct:
+        return LSP.CompletionItemKind.Struct
+      case LSP.SymbolKind.Event:
+        return LSP.CompletionItemKind.Event
+      case LSP.SymbolKind.Operator:
+        return LSP.CompletionItemKind.Operator
+      case LSP.SymbolKind.TypeParameter:
+        return LSP.CompletionItemKind.TypeParameter
+      default:
+        return LSP.CompletionItemKind.Text
+    }
   }
 }
