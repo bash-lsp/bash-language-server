182fecc33d3dfac5f45a744c877a1a3062de37d8
Chris Wendt <chrismwendt@gmail.com>
2018-06-04 18:35:57 -0700

    Fix tests

--- testing/fixtures.ts
+++ testing/fixtures.ts
@@ -1,11 +1,12 @@
 import * as fs from 'fs'
 import * as path from 'path'
+import * as LSP from 'vscode-languageserver'
 
 const base = path.join(__dirname, './fixtures/')
 
 const FIXTURES = {
-  INSTALL: fs.readFileSync(path.join(base, 'install.sh'), 'utf8'),
-  PARSE_PROBLEMS: fs.readFileSync(path.join(base, 'parse-problems.sh'), 'utf8'),
+  INSTALL: LSP.TextDocument.create('foo', 'bar', 0, fs.readFileSync(path.join(base, 'install.sh'), 'utf8')),
+  PARSE_PROBLEMS: LSP.TextDocument.create('foo', 'bar', 0, fs.readFileSync(path.join(base, 'parse-problems.sh'), 'utf8')),
 }
 
 export default FIXTURES
