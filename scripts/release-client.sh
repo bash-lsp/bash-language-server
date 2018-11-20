9c4e31df9a3702b285e1c74bf313453c706d5921
skovhus <kenneth.skovhus@gmail.com>
2018-11-20 17:54:12 +0100

    Run clean before releasing

--- scripts/release-client.sh
+++ scripts/release-client.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 version=$(cat vscode-client/package.json | jq -r .version)
 
-yarn && yarn run check:bail
+yarn run clean
+yarn install
+yarn run check:bail
 
 cd vscode-client && vsce publish ${version}
