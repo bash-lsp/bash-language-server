19c01718dc7e6c985f1eb09d497e723326589776
Remy Suen <remy.suen@gmail.com>
2019-03-14 06:59:46 -0400

    Assign symbol kind for local variables
    Variables inside function were not getting assigned a kind when it was
    being turned into a SymbolInformation type. The absence of this
    required field was causing errors in strict LSP client
    implementations.
    
    Signed-off-by: Remy Suen <remy.suen@gmail.com>

---
+++ testing/fixtures/issue101.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# A simple script with a function...
+
+add_a_user()
+{
+  USER=$1
+  PASSWORD=$2
+  shift; shift;
+  # Having shifted twice, the rest is now comments ...
+  COMMENTS=$@
+  echo "Adding user $USER ..."
+  echo useradd -c "$COMMENTS" $USER
+  echo passwd $USER $PASSWORD
+  echo "Added user $USER ($COMMENTS) with pass $PASSWORD"
+}
+
+###
+# Main body of script starts here
+###
+echo "Start of script..."
+add_a_user bob letmein Bob Holness the presenter
+add_a_user fred badpassword Fred Durst the singer
+add_a_user bilko worsepassword Sgt. Bilko the role model
+echo "End of script..."
