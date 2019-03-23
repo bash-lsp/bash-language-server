fb097f302efb03a83661c4c528d9ec6f9b70353c
skovhus <kenneth.skovhus@gmail.com>
2019-03-23 22:17:01 +0100

    Add glob pattern config

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,6 +3,11 @@ export function getExplainshellEndpoint(): string | null {
   return typeof EXPLAINSHELL_ENDPOINT !== 'undefined' ? EXPLAINSHELL_ENDPOINT : null
 }
 
+export function getGlobPattern(): string {
+  const { GLOB_PATTERN } = process.env
+  return typeof GLOB_PATTERN === 'string' ? GLOB_PATTERN : '**/*@(.sh|.inc|.bash|.command)'
+}
+
 export function getHighlightParsingError(): boolean {
   const { HIGHLIGHT_PARSING_ERRORS } = process.env
   return typeof HIGHLIGHT_PARSING_ERRORS !== 'undefined'
