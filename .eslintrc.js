37e9865df486d01ad41ae93511c870211ab9f6d5
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2019-09-22 20:33:40 +0200

    Switch from tslint to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './server/tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        singleQuote: true,
+        printWidth: 90,
+        semi: false,
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'none',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+      },
+    ],
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'prefer-destructuring': [
+      'error',
+      {
+        array: false,
+        object: true,
+      },
+    ],
+    'prefer-const': 'error',
+    'prefer-template': 'error',
+
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-unused-vars': 'off', // replaced by @typescript-eslint/no-unused-vars
+  },
+  env: {
+    browser: false,
+    node: true,
+    es6: true,
+    'jest/globals': true,
+  },
+}
