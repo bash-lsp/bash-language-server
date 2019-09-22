922e745b77bede76b0ce66e8ca2611c5c0e809c8
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2019-09-22 02:08:21 +0200

    Integrate vscode-client and server without install step

---
+++ vscode-client/src/server.ts
@@ -0,0 +1,44 @@
+import {
+  createConnection,
+  IConnection,
+  InitializeParams,
+  InitializeResult,
+  ProposedFeatures,
+} from 'vscode-languageserver'
+
+import BashLanguageServer from 'bash-language-server'
+
+const connection: IConnection = createConnection(ProposedFeatures.all)
+
+connection.onInitialize(async (params: InitializeParams): Promise<InitializeResult> => {
+  connection.console.info('Initializing Bash LSP server...')
+
+  connection.console.info('Rebuild succeeded')
+
+  const server = await BashLanguageServer.initialize(connection, params)
+
+  connection.console.info('BashLanguageServer initialized, registering connection')
+
+  server.register(connection)
+
+  connection.console.info(
+    'BashLanguageServer initialized ' + JSON.stringify(server.capabilities()),
+  )
+
+  return {
+    capabilities: server.capabilities(),
+  }
+})
+
+// Listen on the connection
+connection.listen()
+
+// Don't die on unhandled Promise rejections
+process.on('unhandledRejection', (reason, p) => {
+  connection.console.error(`Unhandled Rejection at promise: ${p}, reason: ${reason}`)
+})
+
+process.on('SIGPIPE', () => {
+  // Don't die when attempting to pipe stdin to a bad spawn
+  // https://github.com/electron/electron/issues/13254
+})
