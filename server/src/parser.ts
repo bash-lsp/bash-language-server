bd0caf83307028b484d847a016e73603b9428b85
Kenneth Skovhus <kenneth.skovhus@gmail.com>
2019-09-23 21:32:33 +0200

    Update wasm generation instruction

--- server/src/parser.ts
+++ server/src/parser.ts
@@ -4,7 +4,17 @@ export async function initializeParser(): Promise<Parser> {
   await Parser.init()
   const parser = new Parser()
 
-  // NOTE: see https://github.com/tree-sitter/tree-sitter/tree/master/lib/binding_web#generate-wasm-language-files
+  /**
+   * See https://github.com/tree-sitter/tree-sitter/tree/master/lib/binding_web#generate-wasm-language-files
+   *
+   * To compile:
+   *  yarn add --dev tree-sitter-cli
+   *  npx tree-sitter build-wasm node_modules/tree-sitter-bash
+   *
+   * The current files was compiled with:
+   * "tree-sitter-bash": "^0.16.0",
+   * "tree-sitter-cli": "^0.15.9"
+   */
   const lang = await Parser.Language.load(`${__dirname}/../tree-sitter-bash.wasm`)
 
   parser.setLanguage(lang)
