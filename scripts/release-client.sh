8f7c6c5e607fcf409855ff1d740cf36c92df77c2
skovhus <kenneth.skovhus@gmail.com>
2020-03-02 10:20:13 +0100

    Update deploy token name

--- scripts/release-client.sh
+++ scripts/release-client.sh
@@ -6,4 +6,4 @@ yarn run clean
 yarn install
 yarn run verify:bail
 
-cd vscode-client && npx vsce publish -p $VSCE_TOKEN || echo 'Deploy failed'
+cd vscode-client && npx vsce publish -p $VSCE_PERSONAL_ACCESS_TOKEN || echo 'Deploy failed'
