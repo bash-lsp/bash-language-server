3f0a6338cdad8e6fa7db3843efd4ec2b462431c3
skovhus <kenneth.skovhus@gmail.com>
2022-05-14 20:57:36 +0200

    Fix linting issues

--- server/src/util/sh.ts
+++ server/src/util/sh.ts
@@ -107,6 +107,7 @@ export function formatManOutput(manOutput: string): string {
 /**
  * Only works for one-parameter (serializable) functions.
  */
+/* eslint-disable @typescript-eslint/ban-types */
 export function memorize<T extends Function>(func: T): T {
   const cache = new Map()
 
