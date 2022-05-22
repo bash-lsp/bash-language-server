69f1adff06734667cff73dae253ebfc6c2019785
skovhus <kenneth.skovhus@gmail.com>
2022-05-22 10:10:03 +0200

    Add workaround for node 18 support

--- server/src/parser.ts
+++ server/src/parser.ts
@@ -1,6 +1,15 @@
 import * as Parser from 'web-tree-sitter'
 
+const _global: any = global
+
 export async function initializeParser(): Promise<Parser> {
+  if (_global.fetch) {
+    // NOTE: temporary workaround for emscripten node 18 support.
+    // emscripten is used for compiling tree-sitter to wasm.
+    // https://github.com/emscripten-core/emscripten/issues/16915
+    delete _global.fetch
+  }
+
   await Parser.init()
   const parser = new Parser()
 
