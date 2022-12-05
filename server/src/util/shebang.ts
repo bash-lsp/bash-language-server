fada0585cd3118fc985b4fad2c221ea455ae6b36
skovhus <kenneth.skovhus@gmail.com>
2022-12-05 22:50:28 +0100

    Allow more bash dialects

--- server/src/util/shebang.ts
+++ server/src/util/shebang.ts
@@ -1,8 +1,8 @@
 const SHEBANG_REGEXP = /^#!(.*)/
+const SHELL_REGEXP = /bin[/](?:env )?(\w+)/
 
-// TODO: at some point we could let this return all known shells (like dash, ksh, etc)
-// and make the call side decide what to support.
-type SupportedBashDialect = 'bash' | 'sh'
+const BASH_DIALECTS = ['sh', 'bash', 'dash', 'ksh'] as const
+type SupportedBashDialect = typeof BASH_DIALECTS[number]
 
 export function getShebang(fileContent: string): string | null {
   const match = SHEBANG_REGEXP.exec(fileContent)
@@ -14,16 +14,12 @@ export function getShebang(fileContent: string): string | null {
 }
 
 export function getShellDialect(shebang: string): SupportedBashDialect | null {
-  if (shebang.startsWith('/bin/sh') || shebang.startsWith('/usr/bin/env sh')) {
-    return 'sh'
-  }
-
-  if (
-    shebang.startsWith('/bin/bash') ||
-    shebang.startsWith('/usr/bin/bash') ||
-    shebang.startsWith('/usr/bin/env bash')
-  ) {
-    return 'bash'
+  const match = SHELL_REGEXP.exec(shebang)
+  if (match && match[1]) {
+    const bashDialect = match[1].trim() as any
+    if (BASH_DIALECTS.includes(bashDialect)) {
+      return bashDialect
+    }
   }
 
   return null
