1874c0de0008aae88b01b14c4bc89f5ec5cb5e34
babaric-dev <104625126+babaric-dev@users.noreply.github.com>
2023-02-07 22:12:32 +0800

    Add fallback mechanism if `pkg-config` fails
    `COMPLETIONSRC`: path to sourced `bash_completion`
    
    If `pkg-config` fails to get `bash_completion` script:
    - use `brew` on macOS
    - use `PREFIX` on Linux

--- server/src/get-options.sh
+++ server/src/get-options.sh
@@ -1,15 +1,30 @@
 #!/usr/bin/env bash
 
+# Try and get COMPLETIONSRC using pkg-config
 COMPLETIONSDIR="$(pkg-config --variable=completionsdir bash-completion)"
-DATADIR="$(dirname "$(dirname "${COMPLETIONSDIR}")")"
 
-# Exit if bash-completion isn't installed.
-if (( $? != 0 ))
+if (( $? == 0 ))
+then
+	COMPLETIONSRC="$(dirname "$COMPLETIONSDIR")/bash_completion"
+else
+	# Fallback if pkg-config fails
+	if [ "$(uname -s)" = "Darwin" ]
+	then
+		# Running macOS
+		COMPLETIONSRC="$(brew --prefix)/etc/bash_completion"
+	else
+		# Suppose running Linux
+		COMPLETIONSRC="${PREFIX:-/usr}/share/bash-completion/bash_completion"
+	fi
+fi
+
+# Validate path of COMPLETIONSRC
+if (( $? != 0 )) || [ ! -r "$COMPLETIONSRC" ]
 then
 	exit 1
 fi
 
-source "$DATADIR/bash-completion/bash_completion"
+source "$COMPLETIONSRC"
 
 COMP_LINE="$*"
 COMP_WORDS=("$@")
