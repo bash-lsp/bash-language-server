b776c7f57a6de04b05120f80789fbb0de38c7db9
skovhus <kenneth.skovhus@gmail.com>
2023-03-05 21:11:34 +0100

    Remove deprecated tag causing strikethrough

--- server/src/shellcheck/config.ts
+++ server/src/shellcheck/config.ts
@@ -6,12 +6,7 @@ export const SUPPORTED_BASH_DIALECTS = ['sh', 'bash', 'dash', 'ksh']
 
 // https://github.com/koalaman/shellcheck/wiki
 export const CODE_TO_TAGS: Record<number, LSP.DiagnosticTag[] | undefined> = {
-  2006: [LSP.DiagnosticTag.Deprecated],
-  2007: [LSP.DiagnosticTag.Deprecated],
   2034: [LSP.DiagnosticTag.Unnecessary],
-  2186: [LSP.DiagnosticTag.Deprecated],
-  2196: [LSP.DiagnosticTag.Deprecated],
-  2197: [LSP.DiagnosticTag.Deprecated],
 }
 
 // https://github.com/koalaman/shellcheck/blob/364c33395e2f2d5500307f01989f70241c247d5a/src/ShellCheck/Formatter/Format.hs#L50
