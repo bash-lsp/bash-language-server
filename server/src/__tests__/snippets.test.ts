b7743e176d4b223a5e8a34b1588241aea11d59e8
EmilySeville7cfg <EmilySeville7cfg@gmail.com>
2023-03-30 18:43:41 +1000

    feat: remove redundant test for snippets

--- server/src/__tests__/snippets.test.ts
+++ server/src/__tests__/snippets.test.ts
@@ -1,5 +1,3 @@
-import { MarkupContent } from 'vscode-languageserver'
-
 import { SNIPPETS } from '../snippets'
 
 describe('snippets', () => {
@@ -7,25 +5,5 @@ describe('snippets', () => {
     const labels = SNIPPETS.map((snippet) => snippet.label)
     const uniqueLabels = new Set(labels)
     expect(labels.length).toBe(uniqueLabels.size)
-  })
-
-  SNIPPETS.forEach(({ label, documentation }) => {
-    it(`contains the label in the documentation for "${label}"`, () => {
-      const stringDocumentation = (documentation as MarkupContent)?.value
-      expect(stringDocumentation).toBeDefined()
-      if (stringDocumentation && !label.startsWith('[')) {
-        expect(stringDocumentation).toContain(label)
-        const secondLine = stringDocumentation.split('\n')[1]
-        try {
-          expect(
-            secondLine.startsWith(label) || secondLine.startsWith(`[${label}]`),
-          ).toBe(true)
-        } catch (error) {
-          // eslint-disable-next-line no-console
-          console.error(`Did not start with label: ${label}`, secondLine)
-          throw error
-        }
-      }
-    })
   })
 })
