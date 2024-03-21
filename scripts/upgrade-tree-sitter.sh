da6f5fe27d6a255f1027f8811f57cff90f9e9fb9
Blake Easley <6708446+Jimmyscene@users.noreply.github.com>
2024-03-21 05:04:07 +0000

    Fix pnpm add flags

--- scripts/upgrade-tree-sitter.sh
+++ scripts/upgrade-tree-sitter.sh
@@ -4,7 +4,7 @@ set -euox pipefail
 
 cd server
 pnpm add web-tree-sitter
-pnpm add --dev tree-sitter-cli https://github.com/tree-sitter/tree-sitter-bash
+pnpm add --save-dev tree-sitter-cli https://github.com/tree-sitter/tree-sitter-bash
 npx tree-sitter build-wasm node_modules/tree-sitter-bash
 
 curl 'https://api.github.com/repos/tree-sitter/tree-sitter-bash/commits/master' | jq .commit.url > parser.info
