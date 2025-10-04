bash-lsp/bash-language-server

commit 119f32a8ed55ab7fe22d5291a258618584061af7

Upgrade client/server for vscode extension

vscode-client/src/extension.ts
@@ -1,4 +1,5 @@
 'use strict'
+
 import * as path from 'path'
 import { ExtensionContext, workspace } from 'vscode'
 import {
@@ -6,7 +7,9 @@ import {
   LanguageClientOptions,
   ServerOptions,
   TransportKind,
-} from 'vscode-languageclient'
+} from 'vscode-languageclient/node'
+
+let client: LanguageClient
 
 export async function activate(context: ExtensionContext) {
   const config = workspace.getConfiguration('bashIde')
@@ -56,8 +59,14 @@ export async function activate(context: ExtensionContext) {
   }
 
   const client = new LanguageClient('Bash IDE', 'Bash IDE', serverOptions, clientOptions)
+  client.registerProposedFeatures()
+  try {
+    await client.start()
+  } catch (error) {
+    client.error(`Start failed`, error, 'force')
+  }
+}
 
-  // Push the disposable to the context's subscriptions so that the
-  // client can be deactivated on extension deactivation
-  context.subscriptions.push(client.start())
+export function deactivate() {
+  return client.stop()
 }

vscode-client/src/server.ts
@@ -1,13 +1,12 @@
 import BashLanguageServer from 'bash-language-server'
 import {
   createConnection,
-  IConnection,
   InitializeParams,
   InitializeResult,
   ProposedFeatures,
-} from 'vscode-languageserver'
+} from 'vscode-languageserver/node'
 
-const connection: IConnection = createConnection(ProposedFeatures.all)
+const connection = createConnection(ProposedFeatures.all)
 
 connection.onInitialize(async (params: InitializeParams): Promise<InitializeResult> => {
   connection.console.info('BashLanguageServer initializing...')

commit 181c4781db1b81574ff71aa0acb19999b974339f

fix: properly dispose of the client
this fixes a bug introduced in 119f32a8ed55ab7fe22d5291a258618584061af7
(and somehow not noticed in 6eb243e45a10e8b6496161c904fb5b17bbb1fdcf
????)

vscode-client/src/extension.ts
@@ -9,7 +9,7 @@ import {
   TransportKind,
 } from 'vscode-languageclient/node'
 
-let client: LanguageClient
+let client: LanguageClient | undefined
 
 export const CONFIGURATION_SECTION = 'bashIde' // matching the package.json configuration section
 
@@ -60,7 +60,7 @@ export async function activate(context: ExtensionContext) {
     },
   }
 
-  const client = new LanguageClient('Bash IDE', 'Bash IDE', serverOptions, clientOptions)
+  client = new LanguageClient('Bash IDE', 'Bash IDE', serverOptions, clientOptions)
   client.registerProposedFeatures()
 
   try {
@@ -71,8 +71,5 @@ export async function activate(context: ExtensionContext) {
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined
-  }
-  return client.stop()
+  return client?.stop()
 }