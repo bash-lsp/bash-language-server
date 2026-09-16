c7ecd3fe30cf58b29ea0e96c3cd6c76132445189
skovhus <kenneth.skovhus@gmail.com>
2026-09-16 14:54:28 +0200

    Fix CI publishing and prepare server 5.8.0

--- scripts/release-client.sh
+++ scripts/release-client.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 source ./scripts/tag-release.inc
 
-version=$(cat vscode-client/package.json | jq -r .version)
+version=$(jq -r .version vscode-client/package.json)
 tag="vscode-client-${version}"
 
 pnpm clean
@@ -13,5 +13,5 @@ pnpm verify:bail
 
 cd vscode-client
 
-npx @vscode/vsce@2.32.0 publish --skip-duplicate -p $VSCE_TOKEN
-tagRelease $tag || echo "Tag update failed, likely already exists"
+npx @vscode/vsce@2.32.0 publish --skip-duplicate -p "$VSCE_TOKEN"
+tagRelease "$tag"
